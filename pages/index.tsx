a85c11e7b24a7c452a51015dab04916763c59a63
北沢亮和 <ryowa.kitazawa@gmail.com>
2023-01-07 21:04:12 +0900

    add

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import { Inter } from "@next/font/google";
+import Link from "next/link";
+import Layout from "../components/Layout";
 
-const inter = Inter({ subsets: ["latin"] });
+const Home: NextPage = () => {
+  return <Layout />;
+};
 
-export default function Home() {
-  return <></>;
-}
+export default Home;
