fbed84f2c48446be7d0e3a4f6127920a3d097204
北沢亮和 <ryowa.kitazawa@gmail.com>
2023-01-09 15:32:19 +0900

    add: filter

--- utils/notion.ts
+++ utils/notion.ts
@@ -7,5 +7,21 @@ const DATABASE_ID = process.env.NOTION_DATABASE_ID as string;
 export const fetchPages = async () => {
   return await notion.databases.query({
     database_id: DATABASE_ID,
+    filter: {
+      and: [
+        {
+          property: "isPublic",
+          checkbox: {
+            equals: true,
+          },
+        },
+        {
+          property: "slug",
+          rich_text: {
+            is_not_empty: true,
+          },
+        },
+      ],
+    },
   });
 };
