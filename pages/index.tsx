40218316fccfd829d098e4557f789ae8dede9a7c
北沢亮和 <ryowa.kitazawa@gmail.com>
2023-01-09 14:07:49 +0900

    add: multiSelect

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import Card from "../components/Cards";
+import Card from "../components/Card";
 import Layout from "../components/Layout";
 import { siteConfig } from "../site.config";
 import { IndexProps } from "../types/types";
--- utils/property.ts
+++ utils/property.ts
@@ -1,6 +1,6 @@
 import { error } from "console";
 import { debuglog } from "util";
-import { RichTextType } from "../types/types";
+import { PageType, RichTextType } from "../types/types";
 
 export const getText = (richTextArr: RichTextType[]) => {
   try {
@@ -10,4 +10,28 @@ export const getText = (richTextArr: RichTextType[]) => {
     console.log(err);
   }
   return "";
+};
+
+export const getCover = (cover: PageType["cover"]) => {
+  if (cover && cover.file) return cover.file.url;
+  if (cover && cover.external) return cover.external.url;
+  return "/noimage.png";
+};
+
+export const getDate = (date: { start: string }) => {
+  try {
+    return date.start;
+  } catch (err) {
+    console.error(err);
+  }
+  return "-";
+};
+
+export const getMultiSelect = (multiSelect: [{ name: string }]) => {
+  try {
+    return multiSelect.map((tag) => tag.name);
+  } catch (err) {
+    console.error(err)
+  }
+  return [];
 };
